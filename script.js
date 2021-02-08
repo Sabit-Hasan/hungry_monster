a91958fee696df1e0dcefa9a9f2375ce8ce52f38
Sabit Hasan <hayshin.sabit@gmail.com>
2021-02-08 18:31:34 +0600

    see you

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => displayCard(data));
+// fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+// .then(res => res.json())
+// .then(data => displayCard(data));
 
-const displayCard = data =>{
-    const cards = document.getElementById('cards');
-    for (let i = 0; i < data.categories.length; i++) {
-        const element = data.categories[i];
+// const displayCard = data =>{
+//     const cards = document.getElementById('cards');
+//     for (let i = 0; i < data.categories.length; i++) {
+//         const element = data.categories[i];
 
-        const card = document.createElement('div');
-        card.classList.add('card-container','card');
-        const cardInfo = `
-            <img src="${element.strCategoryThumb}">
-            <div class= 'card-body'>
-                <h3>${element.strCategory}</h3>
-            </div>
-        `;
+//         const card = document.createElement('div');
+//         card.classList.add('card-container','card');
+//         const cardInfo = `
+//             <img src="${element.strCategoryThumb}">
+//             <div class= 'card-body'>
+//                 <h3>${element.strCategory}</h3>
+//             </div>
+//         `;
 
-        card.innerHTML = cardInfo;
-        cards.appendChild(card);
-    }
-};
+//         card.innerHTML = cardInfo;
+//         cards.appendChild(card);
+//     }
+// };
 
-const searchItem = data => {
-    const searchValue = document.getElementById('searchInput').value;
-    const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=' + searchValue;
+const searchItem = () =>{
+    const searchText = document.getElementById('search-item').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
 
     fetch(url)
     .then(res => res.json())
-    .then(data =>searchMeal(data));
-};
+    .then(data => searchMeal(data));
+}
 
 const searchMeal = data =>{
-    if(data.meals != null ){
-    (function() {
-        const searchCard = document.getElementById('search-cards');
-        const childDiv = document.createElement('div');
-        childDiv.classList.add('card-container','card');
-        const newCardInfo = `
-            <img src = "${data.meals[0].strMealThumb}">
-            <div class = "card-body">
-                <h3>${data.meals[0].strMeal}</h3>
-            </div>
-        `;
-        childDiv.innerHTML = newCardInfo;
-        searchCard.appendChild(childDiv);
-    })();
+    if(data.meals != null){
+        (function(){
+            for (let i = 0; i < data.meals.length; i++) {
+                const element = data.meals[i];
+                const searchCard = document.getElementById('search-card');
+                const card = document.createElement('div');
+                card.classList.add('card-container','card');
+                const cardInfo = `
+                <img src="${element.strMealThumb}">
+                <div class= 'card-body'>
+                <h3>${element.strMeal}</h3>
+                </div>
+                `;
+                card.innerHTML = cardInfo;
+                searchCard.appendChild(card);
+            }
+        })();
+    }
+    else{
+         alert("This items is not available.");
+    }
 }
-else{
-    document.write("Egun nai,Jgula ache oi gula search din.");
-}
-};
+
+
 
